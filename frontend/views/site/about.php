db95b11806ce705c10eadd373465942bbbf2faad
ridhoafni <kaummuslimin888@gmail.com>
2019-03-28 11:15:31 +0700

    tambahan

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -13,6 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <br></br>
     <h3><p align="justify">
+                Tambahan
         	    Sistem ini merupakan sebuah sistem yang berjalan pada aplikasi web browser untuk mendiagnosa kerusakan Sepeda Motor Matic Injeksi. Sistem Pakar ini dalam pengambilan keputusannya menggunakan metode Forward Chaining dimana permasalahan akan diselesaikan berdasarkan runut maju. Dalam Pengampilan Kesimpulan dimulai dengan dikumpulkannya gejala-gejala. Solusi untuk penanganan sendiri hanya disarankan pada kerusakan yang tidak kompleks. Untuk permasalahan yang tidak boleh dilakukan sendiri, maka sistem akan menyarankan untuk memperbaiki motor matic Injeksi kepada mekanik di Bengkel Resmi Yamaha.
         </p></h3>
 
